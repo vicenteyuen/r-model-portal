031e78b806e975ce0f0367e12910e4e25c9e2c87
Vicente Yuen <ruanweibiao@modernavenue.com>
2016-10-07 17:48:30 +0800

    编写第一个例子。

--- public/js/app/index.js
+++ public/js/app/index.js
@@ -1,8 +1,33 @@
 /**
  * index page handle
  */
-requirejs(['template/doT', 'three'], function (doT, _three) {
+requirejs(['template/doT', 'three'], function (doT, THREE) {
 
-    console.log(_three.REVISION);
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+
+    var geometry = new THREE.CubeGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function render() {
+        "use strict";
+        requestAnimationFrame(render);
+        cube.rotation.x += 0.1;
+        cube.rotation.y += 0.1;
+        renderer.render(scene, camera);
+    }
+
+    render();
 
 });
