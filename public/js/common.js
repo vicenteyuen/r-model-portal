2758e353e245e4b6c9e811e425d80f7ea33a08b0
visonruan <vison_ruan@126.com>
2016-10-10 10:59:26 +0800

    添加three controller 的 AMD 模块支持

--- public/js/common.js
+++ public/js/common.js
@@ -8,12 +8,27 @@ requirejs.config({
         'template/doT': '/js/lib/doT/doT',
         'three': '/js/lib/three/three.min',
 
+
         'three.loaders.fbx': '/js/lib/three/_loaders/fbx',
-        'three.controls.orbit': '/js/lib/three/_controls/orbit'
+        'three.controls.deviceOrientation': '/js/lib/three/_controls/deviceOrientation',
+        'three.controls.drag': '/js/lib/three/_controls/drag',
+        'three.controls.editor': '/js/lib/three/_controls/editor',
+        'three.controls.firstPerson': '/js/lib/three/_controls/firstPerson',
+        'three.controls.fly': '/js/lib/three/_controls/fly',
+        'three.controls.mouse': '/js/lib/three/_controls/mouse',
+        'three.controls.orbit': '/js/lib/three/_controls/orbit',
+        'three.controls.orthographicTrackball': '/js/lib/three/_controls/orthographicTrackball',
+        'three.controls.pointerLock': '/js/lib/three/_controls/pointerLock',
+        'three.controls.trackball': '/js/lib/three/_controls/trackball',
+        'three.controls.transform': '/js/lib/three/_controls/transform',
+        'three.controls.vr': '/js/lib/three/_controls/vr'
 
     },
     shim: {
-
+        'three.loaders.fbx': {
+            deps: ['three'],
+            exports: 'threeLoad'
+        }
     }
 
 });
